c8f597aa3420192f941c69e3ffac381121420c1a
Jay Kim <jaykim92@gmail.com>
2021-02-23 00:24:01 -0800

    styled draft of front page

--- client/src/pages/handbook.js
+++ client/src/pages/handbook.js
@@ -1,11 +1,28 @@
 import React from "react";
 
+// import components
+import StickyNav from "../components/navbar";
+
+// import react-bootstrap components
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Handbook() {
-    return(
-        <>
-            <h1>test handbook page</h1>
-        </>
-    );
-};
+  return (
+    <>
+      <StickyNav />
+      <Container fluid>
+        <Row>
+          <Col fixed="top">test table of contents</Col>
+          <Col xs={8} style={{marginTop: "2rem"}}>
+            <h1>test content</h1>
+          </Col>
+          <Col>test sidebar</Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
 
 export default Handbook;
