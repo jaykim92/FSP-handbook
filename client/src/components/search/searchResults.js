0cd3d8e085e8f1fa76fdfdf4ee2a865300606d67
Jay Kim <jaykim92@gmail.com>
2021-06-23 19:59:51 -0700

    search cards now are hashlinked to go to specific section

--- client/src/components/search/searchResults.js
+++ client/src/components/search/searchResults.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 
 // import components from react-bootstrap
 import Col from "react-bootstrap/Col";
@@ -8,14 +9,14 @@ import Card from "react-bootstrap/Card";
 function SearchResults({ title, content, category, href }) {
   return (
     <Col xs={12} className="mt-3">
-      <a href={`/handbook/${category}/#${href}`}>
+      <Link to={`/handbook/${category}/#${href}`}>
         <Card style={{ width: "100%", height: "100%", overflow: "hidden" }}>
           <Card.Body>
             <Card.Text>{category} section</Card.Text>
             <Card.Text>{content.slice(0, 400)}...</Card.Text>
           </Card.Body>
         </Card>
-      </a>
+      </Link>
     </Col>
   );
 }
